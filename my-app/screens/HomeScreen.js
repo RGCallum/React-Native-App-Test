8027ef53d766a89215cc7ef88b4ed7f4b0177594
Rguy <rguy1290@gmail.com>
2018-11-26 14:01:31 -0500

    started over 2nd 1st commit

--- my-app/screens/HomeScreen.js
+++ my-app/screens/HomeScreen.js
@@ -42,7 +42,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-        WTF is up World
+              WTF is up World?
             </Text>
           </View>
 
